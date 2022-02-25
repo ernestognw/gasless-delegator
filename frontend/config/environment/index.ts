f6e1c05138995c6b70757df5a4a5a933f9290285
ernestognw <ernestognw@gmail.com>
2022-02-24 19:24:08 -0600

    Added chainID to env variables

--- frontend/config/environment/index.ts
+++ frontend/config/environment/index.ts
@@ -1,3 +1,4 @@
 const tokenAddress = process.env.NEXT_PUBLIC_TOKEN_ADDRESS;
+const chainId = Number(process.env.NEXT_PUBLIC_CHAIN_ID);
 
-export { tokenAddress };
+export { tokenAddress, chainId };
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,10 +1,11 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider, NETWORKS } from "@web3-ui/core";
+import { chainId } from "config/environment";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Provider network={NETWORKS.goerli}>
+    <Provider network={chainId}>
       <Component {...pageProps} />
     </Provider>
   );
