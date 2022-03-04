d202ad5f3f997c03b8f0561aaf7176a932d3fbfe
ernestognw <ernestognw@gmail.com>
2022-03-04 12:38:49 -0600

    Remove web3-ui workaround

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,26 +1,6 @@
-module.exports = () => {
-  /** @type {import('next').NextConfig} */
-  const nextConfig = {
-    reactStrictMode: true,
-    // Ugly ts workaround as per https://github.com/vercel/next.js/issues/5666
-    webpack: (config) => {
-      config.module.rules.forEach((rule) => {
-        if (rule.test) {
-          const ruleContainsTs = rule.test.toString().includes("ts|tsx");
-
-          if (
-            ruleContainsTs &&
-            rule.use &&
-            rule.use.loader === "next-babel-loader"
-          ) {
-            rule.include = undefined;
-          }
-        }
-      });
-
-      return config;
-    },
-  };
-
-  return nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 };
+
+module.exports = nextConfig;
